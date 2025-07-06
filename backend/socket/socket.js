329cca352db31950dc0b669a1340eaf0e5f544ac
Tharun8466 <tharunch05@gmail.com>
2025-07-06 12:26:08 +0530

    changed the url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://tharun-chatapp.onrender.com",
   methods: ["GET", "POST"],
   credentials: true
 }));
@@ -14,7 +14,7 @@ app.use(cors({
 const server=http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:['http://localhost:5173'],
+        origin:['https://tharun-chatapp.onrender.com'],
         methods:["GET","POST"],
 
     }
